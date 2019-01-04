3d0833e900f12b2861e44aed152090f026bf6a8c
ELIZABETH LE <elizabethlemk@yahoo.com>
2019-01-04 20:07:43 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,5 @@
-def consolidate_cart(cart:[])
-consolidated_cart = {}
-  cart.each { |grocery| grocery.each { |product,values| consolidated_cart[product] = {price: values[:price], clearance: values[:clearance], count: cart.count(grocery)} } }
-  consolidated_cart
+def consolidate_cart(cart)
+
 end
 
 def apply_coupons(cart, coupons)
